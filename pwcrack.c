0ad459ba3e442b72c788a8d3f2f11894c42e382d
Krueger <ajkrueger@ieng6-202.ucsd.edu>
2024-10-22 08:39:20 -0700

    pa2

---
+++ pwcrack.c
@@ -0,0 +1,114 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <openssl/sha.h>
+
+uint8_t hex_to_byte(unsigned char h1, unsigned char h2){
+   int i = 0;
+    if (h1 >= '0' && h1 <= '9'){
+        i = h1 - '0';
+        i = i * 16;
+    }
+    else{
+        i = h1 - 'a' + 10;
+        i = i * 16;
+    }
+    if (h2 >= '0' && h2 <= '9'){
+        i += h2 - '0';
+    }
+    else{
+        i += h2 - 'a' + 10;
+    }
+    return i;
+}
+
+void hexstr_to_hash(char hexstr[], unsigned char hash[32]){
+int j = 0;
+for (int i = 0; i < 64; i++){
+hash[i] = hex_to_byte( hexstr[j], hexstr[j+1]);
+j+=2;
+}
+}
+
+
+int8_t check_password(char password[], unsigned char given_hash[32]){
+unsigned char hash[32];
+SHA256(password, strlen(password), hash);
+
+for (int i =0; i < 32; i++){
+    if (given_hash[i] != hash[i]){
+        return 0;
+    }
+}
+
+return 1;
+
+}
+
+
+int8_t crack_password(char password[], unsigned char given_hash[]){
+
+  int j = 0;
+
+    if(check_password(password, given_hash) == 0){
+        char test[strlen(password)];
+        strcpy(test, password);
+        for (int i = 0; i < strlen(password); i++){
+            j=0;
+            if(test[i] >= 'a' && test[i] <= 'z'){
+                test[i] = test[i] - 32;
+                j = 1;
+            }
+            if(check_password(test, given_hash) == 1){
+                password[i] = password[i] - 32;
+                return 1;
+            }else if(j == 1){
+                
+                    test[i] = test[i] + 32;
+                
+              
+            }
+            
+        }
+        for (int i = 0; i < strlen(password); i++){
+            j=0;
+            if(test[i] >= 'A' && test[i] <= 'Z'){
+                j = 1;
+                test[i] = test[i] + 32;
+            }
+            if(check_password(test, given_hash) == 1){
+                password[i] = password[i] + 32;
+                return 1;
+            }
+            else if(j == 1){
+                test[i] = test[i] - 32;
+
+        }
+
+
+    }
+       return 0;
+    }
+return 1;
+}
+    
+
+
+
+int main(){
+
+char password[] = "paSSword";
+char hash_as_hexstr[] = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"; // SHA256 hash of "password"
+unsigned char given_hash[32];
+hexstr_to_hash(hash_as_hexstr, given_hash);
+int8_t match = crack_password(password, given_hash);
+
+printf("%d\n ",match);
+puts(password);
+
+
+
+    return 0;
+}
