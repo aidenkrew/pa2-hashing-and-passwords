27ab23624317a21e50d08fbe7848a90b8ad30cbb
aidenkrew <ajkrueger@ucsd.edu>
2024-10-24 11:34:26 -0700

    Update pwcrack.c

--- pwcrack.c
+++ pwcrack.c
@@ -106,8 +106,8 @@ unsigned char given_hash[32];
 hexstr_to_hash(hash_as_hexstr, given_hash);
 
 
-char password[33];
-char correct_password[33];
+char password[65];
+char correct_password[65];
 int8_t match = 0;
 
 
